3e929d9195dab3f8d5cfad96618ca263a24318f7
Michael Ripley <zkxs00@gmail.com>
2017-02-01 10:35:42 -0600

    Implement packet

--- src/main/java/net/ae97/pokebot/extensions/mcping/protocol/Packet.java
+++ src/main/java/net/ae97/pokebot/extensions/mcping/protocol/Packet.java
@@ -1,14 +1,40 @@
 package net.ae97.pokebot.extensions.mcping.protocol;
 
+import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
 
 import net.ae97.pokebot.extensions.mcping.connection.Manager;
+import net.ae97.pokebot.extensions.mcping.protocol.datatypes.VarInt;
 
 public class Packet {
-    private ByteBuffer dataBuffer;
-    
+    private ByteBuffer data;
+
     public Packet(Manager manager) {
-        dataBuffer = manager.getMemoryManager().allocate();
-        // TODO: this is where you left off
+        data = manager.getMemoryManager().allocate();
+        data.clear();
     }
+
+    public void send(final int packetId, SocketChannel socket) throws IOException {
+        final ByteBuffer header = ByteBuffer.allocateDirect(10); // 10 is the max size of two varints
+
+        // determine the length of the packet id
+        VarInt.write(packetId, header);
+        final int packetIdLength = header.position() - 1;
+
+        header.clear();
+        data.flip(); // prepare data for writing
+        VarInt.write(packetIdLength + data.remaining(), header);
+        VarInt.write(packetId, header);
+        header.flip(); // prepare header for writing
+
+        // write packet to socket
+        socket.write(header);
+        socket.write(data);
+    }
+
+    public ByteBuffer getDataBuffer() {
+        return data;
+    }
+
 }
